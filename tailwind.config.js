33ea0d554372fb8424d01e4d25fc0f6ec4caff44
sam-12srivastava <sk75043@gmail.com>
2025-02-07 18:01:29 +0530

    upload files

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        mullish: ["Mulish", "sans-serif"],
+      },
+      colors:{
+deepBlue: "#02042a",
+lightBlue:"#2b84ea",
+lightBlue300:"#0b72e7",
+greenLight:"#61cea6",
+grayText:"#818597",
+lightGray:"#e2e2e2",
+greenBlue:"#344a6c",
+deepBluehead:"#162f56",
+gray2:"#525a76",
+      }
+    },
+  },
+  plugins: [],
+}
+
